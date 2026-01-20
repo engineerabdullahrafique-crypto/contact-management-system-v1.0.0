c89b46497713b1779e42e574f5b2eda4595f80e4
MuhammadAbdullah <engineer.abdullahrafique@gmail.com>
2026-01-20 15:21:39 +0500

    setup the project

---
+++ backend/src/test/java/com/ab/cmdBackend/ContactManangementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ab.cmdBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactManangementSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
