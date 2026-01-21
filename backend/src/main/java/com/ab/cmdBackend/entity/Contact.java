812cd69a1565bde6c3ead20a416648852398586c
MuhammadAbdullah <engineer.abdullahrafique@gmail.com>
2026-01-21 14:41:53 +0500

    Initial project with entity classes

---
+++ backend/src/main/java/com/ab/cmdBackend/entity/Contact.java
@@ -0,0 +1,23 @@
+package com.ab.cmdBackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Contact {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String title;
+    private String emailWork;
+    private String emailPersonal;
+    private String phoneWork;
+    private String phoneHome;
+    private String phonePersonal;
+
+    @ManyToOne
+    private User user;
+}
---
+++ backend/src/main/java/com/ab/cmdBackend/entity/User.java
@@ -0,0 +1,16 @@
+package com.ab.cmdBackend.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users") // Avoid conflict with reserved word
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String email;
+    private String phone;
+    private String password;
+}
